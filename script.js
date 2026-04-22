83bea1d89bb2053bfb0d57318ba1e8bb498ca49b
humatahreem-tech <humatahreem5@gmail.com>
2026-04-22 12:02:41 +0530

    Remove login function and related elements
    Removed login functionality and associated event listener.

--- script.js
+++ script.js
@@ -7,37 +7,9 @@ const foot = document.getElementById('footer');
 const hero = document.getElementById('Hero-section');
 const data = document.getElementById('Data');
 const about = document.getElementById('About-me');
-const loginpage = document.getElementById('Login-page');
-
-function login(event){
-  event.preventDefault();
-let email = document.getElementById('email');  let pass = document.getElementById('pass');
-
- const mail = email.value;
- const lock = pass.value;
-
-  if(mail==="" || lock===""){
-    alert('please fill in all the details !')
-  }else if(mail.includes('@') &&   mail.includes('.') && lock.length >= 6 ){
-    alert('login successful !')
-  
-  loginpage.style.display = 'none';
-  hero.style.display = 'flex';
-  head.style.display = 'flex';
-  foot.style.display = 'block';
-  }else{
-    alert('Invalid email or password please try again !')
-  }
-}            
-
-btn.addEventListener('click', login);
-
-console.log(login);
 
 function me(event){
   event.preventDefault();
-
-loginpage.style.display = 'none';
   hero.style.display = 'none';
   data.style.display = 'none';
   about.style.display = 'flex';
@@ -48,15 +20,11 @@ intro.addEventListener('click', me);
 
 function landing(event){
   event.preventDefault();
-
-loginpage.style.display = 'none';
   about.style.display = 'none';
   data.style.display = 'none';
   hero.style.display = 'flex';
 }
 home.addEventListener('click', landing);   
-
-
 
 const findBtn = document.getElementById('find-btn'); 
 
